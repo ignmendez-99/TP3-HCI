5e6fd19b97338862d2934a43566292b992855722
Ignacio Mendez <ignmendez@itba.edu.ar>
2020-06-17 10:01:58 -0300

    Started preparing things for using RETROFIT

---
+++ UltraHome/app/src/main/java/com/example/ultrahome/apiConnection/Result.java
@@ -0,0 +1,26 @@
+package com.example.ultrahome.apiConnection;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result<T> {
+
+    @SerializedName("result")
+    @Expose
+    private T result = null;
+
+    public Result() {
+    }
+
+    public Result(T result) {
+        this.result = result;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+}
