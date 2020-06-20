45c9725e70d922ecc6560988605a62e167fe616b
Juan <juangriggio@gmail.com>
2020-06-20 16:16:47 -0300

    Minor improvements

--- UltraHome/app/src/main/java/com/example/ultrahome/ui/devices/controllers/BlindsControllerFragment.java
+++ UltraHome/app/src/main/java/com/example/ultrahome/ui/devices/controllers/BlindsControllerFragment.java
@@ -1,15 +1,23 @@
 package com.example.ultrahome.ui.devices.controllers;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.ultrahome.MainActivity;
 import com.example.ultrahome.R;
 import com.example.ultrahome.ui.devices.DevicesListFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,11 +27,17 @@ public class BlindsControllerFragment extends Fragment {
     private Button deleteButton;
     private FloatingActionButton editButton;
     private FloatingActionButton doneButton;
+    private EditText nameEdited;
+    private TextView name;
+
+    private String currentName = "PRUEBA";
 
     private String deviceId;
     private int positionInRecyclerView;
 
     private boolean editing = false;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_blinds_controller, container, false);
@@ -36,7 +50,7 @@ public class BlindsControllerFragment extends Fragment {
         readBundle(getArguments());
 
         deleteButton = view.findViewById(R.id.delete_button);
-        deleteButton.setOnClickListener(this::deleteDevice);
+        deleteButton.setOnClickListener(this::deletePressed);
         deleteButton.setVisibility(View.INVISIBLE);
         deleteButton.setClickable(false);
 
@@ -47,6 +61,14 @@ public class BlindsControllerFragment extends Fragment {
 
         editButton = view.findViewById(R.id.edit_button);
         editButton.setOnClickListener(this::editPressed);
+
+        nameEdited = view.findViewById(R.id.name_edited);
+        nameEdited.setVisibility(View.INVISIBLE);
+
+        name = view.findViewById(R.id.name);
+        name.setText(currentName);
+
+        nameEdited.setText(currentName);
 
 
     }
@@ -59,6 +81,14 @@ public class BlindsControllerFragment extends Fragment {
             deleteButton.setClickable(false);
             doneButton.setVisibility(View.INVISIBLE);
             doneButton.setClickable(false);
+            nameEdited.setVisibility(View.INVISIBLE);
+            name.setVisibility(View.VISIBLE);
+
+            if (! nameEdited.getText().toString().equals(currentName)) {
+                currentName = nameEdited.getText().toString();
+                name.setText(currentName);
+                // API CALL PARA RENOMBRAR
+            }
         } else {
             editButton.setVisibility(View.INVISIBLE);
             editButton.setClickable(false);
@@ -66,8 +96,18 @@ public class BlindsControllerFragment extends Fragment {
             deleteButton.setClickable(true);
             doneButton.setVisibility(View.VISIBLE);
             doneButton.setClickable(true);
+            nameEdited.setVisibility(View.VISIBLE);
+            name.setVisibility(View.INVISIBLE);
         }
         editing = !editing;
+    }
+
+    private void deletePressed(View view) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        // Create and show the dialog.
+        ConfirmationDialog newFragment = new ConfirmationDialog ();
+        newFragment.show(ft, "dialog");
+
     }
 
     private void deleteDevice(View view) {
---
+++ UltraHome/app/src/main/java/com/example/ultrahome/ui/devices/controllers/ConfirmationDialog.java
@@ -0,0 +1,31 @@
+package com.example.ultrahome.ui.devices.controllers;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ConfirmationDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Warning!")
+                .setMessage("Are you sure you want to delete ")
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing (will close dialog)
+                    }
+                })
+                .setPositiveButton(android.R.string.yes,  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do something
+                    }
+                })
+                .create();
+    }
+}
