7db4bbfa9e26c0043481549f4ff4c034572222e0
Franco Navarro <frnavarro@itba.edu.ar>
2020-06-27 16:53:41 -0300

    Auto stash before merge of "development" and "origin/development"

--- UltraHome/app/src/main/java/com/example/ultrahome/ui/devices/DevicesListFragment.java
+++ UltraHome/app/src/main/java/com/example/ultrahome/ui/devices/DevicesListFragment.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
